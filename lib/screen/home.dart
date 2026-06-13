7262e40fb9d1e1458f95913624f94276a075f868
walid <yarlookbrosez30@gmail.com>
2026-06-13 10:44:29 +0700

    Step 6 : Image Widget

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -6,20 +6,18 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.purpleAccent,
-        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        padding: EdgeInsets.all(50),
-        child: Text(
-          "Hello World 4",
-          style: TextStyle(
-            fontSize: 30, 
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Image.asset('assets/images/flower.jpg', width: 200, height: 200,),
+        SizedBox(height: 20),
+        Image.network(
+          'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg',
+          width: 200,
+          height: 200,
         ),
-      ),
+      ],
     );
   }
 }
