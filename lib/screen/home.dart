21c3308e548bbec4c594c1c311b362443f38da37
walid <yarlookbrosez30@gmail.com>
2026-06-13 11:28:23 +0700

    Step 6: Button Widget

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -6,18 +6,51 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Image.asset('assets/images/flower.jpg', width: 200, height: 200,),
-        SizedBox(height: 20),
-        Image.network(
-          'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg',
-          width: 200,
-          height: 200,
-        ),
-      ],
-    );
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(
+            style: TextButton.styleFrom(foregroundColor: Colors.pinkAccent),
+            onPressed: () {
+              print("กดปุ่มแล้ว");
+            },
+            child: Text(
+              "กดปุ่มนี้",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ), 
+          ),
+          const SizedBox(height: 20), // เพิ่มระยะห่างระหว่างปุ่ม
+          FilledButton(
+            style: FilledButton.styleFrom(
+              backgroundColor: Colors.purpleAccent,
+              foregroundColor: Colors.white,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            ),
+            onPressed: () {
+              print("กดปุ่มแล้ว FilledButton");
+            },
+            child: Text(
+              "กดปุ่มนี้",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ), 
+          ),
+          const SizedBox(height: 20), // เพิ่มระยะห่างระหว่างปุ่ม
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 233, 255, 68),
+              foregroundColor: Colors.white,
+            ),
+            onPressed: () {
+              print("กดปุ่มแล้ว ElevatedButton");
+            },
+            child: Text(
+              "กดปุ่มนี้",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          )
+        ],
+      ), // Column
+    ); // Center
   }
 }
